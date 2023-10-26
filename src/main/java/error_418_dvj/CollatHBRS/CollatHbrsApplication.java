497477dd417575c5faf6945eda2a5ee262179431
Lorin Kanoglu <lorin.kanoglu@smail.inf.h-brs.de>
2023-10-26 11:16:20 +0200

    Initial commit

---
+++ src/main/java/error_418_dvj/CollatHBRS/CollatHbrsApplication.java
@@ -0,0 +1,13 @@
+package error_418_dvj.CollatHBRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollatHbrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollatHbrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/error_418_dvj/CollatHBRS/CollatHbrsApplicationTests.java
@@ -0,0 +1,13 @@
+package error_418_dvj.CollatHBRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollatHbrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
