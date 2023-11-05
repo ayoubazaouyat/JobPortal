5cb3e03256898ba8d2ad09423c39d3e6aa338e8a
Markus <markus.frenken@smail.inf.h-brs.de>
2023-11-05 17:19:01 +0100

    Add comments, setters for id and username-field with getters/setters

--- src/main/java/error_418_dvj/CollatHBRS/Backend/Student.java
+++ src/main/java/error_418_dvj/CollatHBRS/Backend/Student.java
@@ -6,22 +6,32 @@ import jakarta.persistence.*;
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "matrNr") // Nutze matrNr als Primaerschluessel
+    @Column(name = "matrNr") // Use 'matrNr' as Primary Key
+    /* matrNr is annotated by '@Id' which tells our persistence provider to access our field 'matrNr' directly.
+    Therefore, we don't need to annotate our getters and setters for the id.
+    */
     private Long matrNr;
     private String name;
     private String vorname;
+    private String username;
     private String email;
 
     protected Student() {}
 
-    public Student(String name, String vorname, String email) {
+    public Student(String name, String vorname, String username, String email) {
         this.name = name;
         this.vorname = vorname;
+        this.username = username;
         this.email = email;
     }
 
     public Long getId() {
         return matrNr;
+    }
+
+    // Make this setter protected so other methods do not interfere with JPA
+    protected void setId(Long matrNr) {
+        this.matrNr = matrNr;
     }
 
     public String getName() {
@@ -38,6 +48,14 @@ public class Student {
 
     public void setVorname(String vorname) {
         this.vorname = vorname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.vorname = username;
     }
 
     public String getEmail() {
--- src/main/java/error_418_dvj/CollatHBRS/Backend/Unternehmen.java
+++ src/main/java/error_418_dvj/CollatHBRS/Backend/Unternehmen.java
@@ -6,21 +6,31 @@ import jakarta.persistence.*;
 public class Unternehmen {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column (name = "unternehmensId") // Nutze unternehmensId als Primaerschluessel
-
+    @Column (name = "unternehmensId") // Use 'unternehmensId' as Primary Key
+    /* unternehmensId is annotated by '@Id' which tells our persistence provider to
+    access our field 'unternehmensId' directly.
+    Therefore, we don't need to annotate our getters and setters for the id.
+    */
     private Long unternehmensId;
     private String name;
+    private String username;
     private String email;
 
     protected Unternehmen() {}
 
-    public Unternehmen(String name, String email) {
+    public Unternehmen(String name, String username, String email) {
         this.name = name;
+        this.username = username;
         this.email = email;
     }
 
     public Long getId() {
         return unternehmensId;
+    }
+
+    // Make this setter protected so other methods do not interfere with JPA
+    protected void setId(Long unternehmensId) {
+        this.unternehmensId = unternehmensId;
     }
 
     public String getName() {
@@ -29,6 +39,14 @@ public class Unternehmen {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUsername(String username) {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
