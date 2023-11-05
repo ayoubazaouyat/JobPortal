4841b68b7efc7aca6a800be0e919f3024ebacd60
Markus <markus.frenken@smail.inf.h-brs.de>
2023-11-05 16:40:38 +0100

    Add JPA-Repository-Class for Unternehmen

---
+++ src/main/java/error_418_dvj/CollatHBRS/Backend/UnternehmenRepository.java
@@ -0,0 +1,6 @@
+package error_418_dvj.CollatHBRS.Backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnternehmenRepository extends JpaRepository<Unternehmen, Long> {
+}
