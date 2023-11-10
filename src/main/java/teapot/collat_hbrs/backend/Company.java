254b5f1802fee22a73b742743accfe484ba1e9fa
Lorin Kanoglu <lorin.kanoglu@smail.inf.h-brs.de>
2023-11-10 14:35:40 +0100

    Added class jobAdvertisement and upgraded class Company

--- src/main/java/teapot/collat_hbrs/backend/Company.java
+++ src/main/java/teapot/collat_hbrs/backend/Company.java
@@ -1,12 +1,18 @@
 package teapot.collat_hbrs.backend;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 @Entity
 @DiscriminatorValue("C")
 public class Company extends Account{
     private String name;
     private String email;
+
+    private List<jobAdvertisement> jobAdvertisements = new ArrayList<>();
 
     protected Company() {}
 
@@ -30,4 +36,27 @@ public class Company extends Account{
     public void setEmail(String email) {
         this.email = email;
     }
+
+
+
+    public void setJobAdvertisements(List<jobAdvertisement> jobAdvertisements) {
+        this.jobAdvertisements = jobAdvertisements;
+    }
+
+    public void postJobAdvertisement(jobAdvertisement x) {
+        jobAdvertisements.add(x);
+    }
+
+    public void deleteJobAdvertisement(jobAdvertisement x) {
+        jobAdvertisements.remove(x);
+    }
+
+
+    // um wichtige Stellenanzeigen hervorzuheben
+    public void pushJobAdvertisement(jobAdvertisement x) {
+        deleteJobAdvertisement(x);
+        jobAdvertisements.add(0, x);
+    }
+
 }
+
