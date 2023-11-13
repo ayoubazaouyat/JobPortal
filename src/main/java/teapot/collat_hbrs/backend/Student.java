81ea5d099844cf9597b4699e8af29a4899adf357
Samar reguigui <reguiguisamar4@gmail.com>
2023-11-13 21:04:07 +0100

    Pflicht Felder

--- src/main/java/teapot/collat_hbrs/backend/Student.java
+++ src/main/java/teapot/collat_hbrs/backend/Student.java
@@ -2,6 +2,8 @@ package teapot.collat_hbrs.backend;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @DiscriminatorValue("S")
 public class Student extends Account {
@@ -9,6 +11,16 @@ public class Student extends Account {
     private String forename;
     private String email;
 
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+    private String city;
+    private String phone;
+    private String studyProgram;
+
+
+    @ElementCollection
+    private List<String> skills;
     protected Student() {
     }
 
@@ -17,6 +29,14 @@ public class Student extends Account {
         this.forename = forename;
         this.username = username;
         this.email = email;
+
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.phone = phone;
+        this.studyProgram = studyProgram;
+        this.skills = skills;
     }
 
     public String getSurname() {
@@ -41,6 +61,62 @@ public class Student extends Account {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStudyProgram() {
+        return studyProgram;
+    }
+
+    public void setStudyProgram(String studyProgram) {
+        this.studyProgram = studyProgram;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
     }
 }
 
