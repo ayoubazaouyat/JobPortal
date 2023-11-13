5270efc888e5a6ad29f77f860e547d85170aee21
Markus <markus.frenken@smail.inf.h-brs.de>
2023-11-13 20:57:28 +0100

    Add attributes needed for the company profile

--- src/main/java/teapot/collat_hbrs/backend/Company.java
+++ src/main/java/teapot/collat_hbrs/backend/Company.java
@@ -9,6 +9,13 @@ import java.util.List;
 @DiscriminatorValue("C")
 public class Company extends Account{
     private String name;
+    private String streetName;
+    private String houseNumber; // Use String because houseNumber could be 41a or similiar
+    private String postalCode;
+    private String city;
+    private String industry; // Branche
+    private String companyDescription;
+    private String landlineNumber;
     private String email;
 
 
@@ -17,9 +24,18 @@ public class Company extends Account{
 
     protected Company() {}
 
-    public Company(String name, String username, String email) {
+    public Company(String name, String username, String streetName, String houseNumber,
+                   String postalCode, String city, String industry, String companyDescription,
+                   String landlineNumber, String email) {
         this.name = name;
         this.username = username;
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.industry = industry;
+        this.companyDescription = companyDescription;
+        this.landlineNumber = landlineNumber;
         this.email = email;
     }
     public String getName() {
@@ -28,6 +44,62 @@ public class Company extends Account{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getCompanyDescription() {
+        return companyDescription;
+    }
+
+    public void setCompanyDescription(String companyDescription) {
+        this.companyDescription = companyDescription;
+    }
+
+    public String getLandlineNumber() {
+        return landlineNumber;
+    }
+
+    public void setLandlineNumber(String landlineNumber) {
+        this.landlineNumber = landlineNumber;
     }
 
     public String getEmail() {
