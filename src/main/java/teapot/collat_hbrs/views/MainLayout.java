d2d1b2b0ea3d6686f13321a5846776dd63c1343d
Ayoub Azaouyat <ayoub.azaouyat@smail.inf.h-brs.de>
2023-11-23 11:35:33 +0100

    remove helloview

--- src/main/java/teapot/collat_hbrs/views/MainLayout.java
+++ src/main/java/teapot/collat_hbrs/views/MainLayout.java
@@ -76,7 +76,6 @@ public class MainLayout extends AppLayout {
     private SideNav createNavigation() {
         SideNav nav = new SideNav();
 
-        nav.addItem(new SideNavItem("Hello World", HelloWorldView.class, new Icon(VaadinIcon.CLOSE_CIRCLE)));
         nav.addItem(new SideNavItem("Job Search", JobSearchView.class, new Icon(VaadinIcon.NOTEBOOK)));
         nav.addItem(new SideNavItem("Company Search", CompanySearchView.class, new Icon(VaadinIcon.BUILDING)));
         nav.addItem(new SideNavItem("About Us", AboutUsView.class, new Icon(VaadinIcon.INFO_CIRCLE)));
