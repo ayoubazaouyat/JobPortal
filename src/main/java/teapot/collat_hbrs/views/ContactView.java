f4f65d391cdc43e3a9ddf6ab40f927cf7b85ce74
Markus <markus.frenken@smail.inf.h-brs.de>
2024-01-13 16:48:24 +0100

    Remove contact buttons for students and companies

--- src/main/java/teapot/collat_hbrs/views/ContactView.java
+++ src/main/java/teapot/collat_hbrs/views/ContactView.java
@@ -24,64 +24,58 @@ import teapot.collat_hbrs.frontend.Format;
 public class ContactView extends Composite<VerticalLayout> {
 
     public ContactView() {
-        H2 h2 = new H2();
         HorizontalLayout layoutRow = new HorizontalLayout();
-        Button buttonPrimary = new Button();
-        Button buttonPrimary2 = new Button();
         VerticalLayout layoutColumn2 = new VerticalLayout();
-        H2 h22 = new H2();
-        H4 h4 = new H4();
         VerticalLayout layoutColumn3 = new VerticalLayout();
+
+        H2 h2 = new H2();
         H2 h23 = new H2();
+        H4 h4 = new H4();
         H4 h42 = new H4();
+
         Paragraph textMedium = new Paragraph();
         getContent().setWidth("100%");
         getContent().setHeight("130px");
-        h2.setText("Online Contact");
-        h2.setWidth(Format.MAX_CONTENT);
+
         layoutRow.setWidthFull();
         getContent().setFlexGrow(1.0, layoutRow);
         layoutRow.addClassName(Gap.MEDIUM);
         layoutRow.setWidth("100%");
         layoutRow.getStyle().set(Format.FLEX_GROW, "1");
-        buttonPrimary.setText("for Students");
-        buttonPrimary.setWidth("153px");
-        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonPrimary2.setText("for Companies");
-        buttonPrimary2.setWidth("153px");
-        buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
         layoutColumn2.setWidthFull();
         getContent().setFlexGrow(1.0, layoutColumn2);
         layoutColumn2.setWidth("100%");
         layoutColumn2.getStyle().set(Format.FLEX_GROW, "1");
-        h22.setText("Customer Hotline");
-        h22.setWidth(Format.MAX_CONTENT);
-        h4.setText("01234 / 567890");
-        h4.setWidth(Format.MAX_CONTENT);
+
         layoutColumn3.setWidthFull();
         getContent().setFlexGrow(1.0, layoutColumn3);
         layoutColumn3.setWidth("100%");
         layoutColumn3.getStyle().set(Format.FLEX_GROW, "1");
-        h23.setText("Contact");
-        h23.setWidth(Format.MAX_CONTENT);
-        h42.setText("coll@hbrs");
-        h42.setWidth(Format.MAX_CONTENT);
-        textMedium.setText(
-                "Musterstraße 1                                                                                                                                                                                                                              12345 Musterstadt                                                                                                                                                                                                                    Phone: 01234 / 567890                                                                                                                                                                                                              E-Mail: max@mustermann.de");
-        textMedium.setWidth("100%");
-        textMedium.setHeight("100px");
-        textMedium.getStyle().set("font-size", "var(--lumo-font-size-m)");
+
         Button buttonPrimary3 = new Button();
         buttonPrimary3.setText("Back");
         buttonPrimary3.setWidth(Format.MIN_CONTENT);
         buttonPrimary3.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         buttonPrimary3.addClickListener(buttonClickEvent -> UI.getCurrent().navigate("/"));
-        getContent().add(h2);
+
+        h2.setText("Customer Hotline");
+        h2.setWidth(Format.MAX_CONTENT);
+        h4.setText("01234 / 567890");
+        h4.setWidth(Format.MAX_CONTENT);
+
+        h23.setText("Contact");
+        h23.setWidth(Format.MAX_CONTENT);
+        h42.setText("coll@hbrs");
+        h42.setWidth(Format.MAX_CONTENT);
+        textMedium.setText("Musterstraße 1 12345 Musterstadt Phone: 01234 / 567890 E-Mail: max@mustermann.de");
+        textMedium.setWidth("100%");
+        textMedium.setHeight("100px");
+        textMedium.getStyle().set("font-size", "var(--lumo-font-size-m)");
+
         getContent().add(layoutRow);
-        layoutRow.add(buttonPrimary);
-        layoutRow.add(buttonPrimary2);
         getContent().add(layoutColumn2);
-        layoutColumn2.add(h22);
+        layoutColumn2.add(h2);
         layoutColumn2.add(h4);
         getContent().add(layoutColumn3);
         layoutColumn3.add(h23);
