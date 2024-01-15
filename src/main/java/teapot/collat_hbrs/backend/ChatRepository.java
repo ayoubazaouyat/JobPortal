61707d956c9b185cc2a391f40fea08326b26ceb1
Markus <markus.frenken@smail.inf.h-brs.de>
2024-01-16 00:18:02 +0100

    Fix repo find method

--- src/main/java/teapot/collat_hbrs/backend/ChatRepository.java
+++ src/main/java/teapot/collat_hbrs/backend/ChatRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findByCompanyName(String companyName);
-    List<ChatMessage> findByStudentName(String studentName);
+    List<ChatMessage> findBySender(String sender);
 }
