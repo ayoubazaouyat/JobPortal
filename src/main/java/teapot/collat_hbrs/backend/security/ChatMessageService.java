f6cf881321299df4ff951e7fe63a0819c02f86a4
Markus <markus.frenken@smail.inf.h-brs.de>
2024-01-16 00:03:51 +0100

    Add service for chat messages

---
+++ src/main/java/teapot/collat_hbrs/backend/security/ChatMessageService.java
@@ -0,0 +1,51 @@
+package teapot.collat_hbrs.backend.security;
+
+import org.springframework.stereotype.Service;
+import teapot.collat_hbrs.backend.ChatMessage;
+import teapot.collat_hbrs.backend.ChatRepository;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChatMessageService {
+
+    private final ChatRepository chatRepository;
+    public static List<ChatMessage> allChatMessages = new ArrayList<>();
+
+    public ChatMessageService(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+    public List<ChatMessage> getAllChatMessages() {
+        return chatRepository.findAll();
+    }
+
+    public void addToAllChatMessages(ChatMessage chatMessage) {
+        allChatMessages.add(chatMessage);
+    }
+
+    public ChatMessage addChatMessage(ChatMessage chatMessage) {
+        if (chatMessage.getRecipient() == null || chatMessage.getRecipient().trim().isEmpty()) {
+            throw new IllegalArgumentException("Please assign a recipient");
+        } else if (chatMessage.getSubject() == null || chatMessage.getSubject().trim().isEmpty()) {
+            throw new IllegalArgumentException("Please assign a subject");
+        } else {
+            return chatRepository.save(chatMessage);
+        }
+    }
+
+    public void deleteChatMessage(ChatMessage chatMessage) {
+        chatRepository.delete(chatMessage);
+    }
+
+    public List<ChatMessage> getChatMessageForCompany(String companyName) {
+        return chatRepository.findByCompanyName(companyName);
+    }
+
+    public List<ChatMessage> getChatMessageForStudent(String surname) {
+        return chatRepository.findByStudentName(surname);
+    }
+
+
+
+}
